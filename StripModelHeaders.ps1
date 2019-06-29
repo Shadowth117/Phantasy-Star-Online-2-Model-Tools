ccdd9a452e326b18ac06423b6847966f284344b4
Shadowth117 <shadic@msn.com>
2019-06-29 01:11:26 -0400

    Update StripModelHeaders.ps1
    Edit by LilG.

--- StripModelHeaders.ps1
+++ StripModelHeaders.ps1
@@ -1,17 +1,34 @@
-Get-ChildItem (Split-Path $MyInvocation.MyCommand.Path) -Filter *.trp | 
-Foreach-Object {
-$bytes = [System.IO.File]::ReadAllBytes($_.FullName);
+param(
+	[System.IO.DirectoryInfo]$pathParam = $null
+)
 
-$bytesOut = $bytes[64..($bytes.Count-1)];
+[System.IO.DirectoryInfo]$path
 
-[System.IO.File]::WriteAllBytes(($_.FullName + '.aqo'), $bytesOut);
+if($pathParam) {
+    $path = (Get-Item -Path $pathParam)
+    if($path -isnot [System.IO.DirectoryInfo]) { 
+		throw [System.IO.DirectoryNotFoundException]::new("Path is not a directory: $pathParam") 
+	}
+} else {
+    [System.Management.Automation.PathInfo] $path = (Get-Location)
 }
 
-Get-ChildItem (Split-Path $MyInvocation.MyCommand.Path) -Filter *.aqp | 
-Foreach-Object {
-$bytes = [System.IO.File]::ReadAllBytes($_.FullName);
+Get-ChildItem -Path $path -Filter *.trp | 
+	Foreach-Object {
+		$PSItem.FullName
+		$bytes = [System.IO.File]::ReadAllBytes($PSItem.FullNupdaame);
+		
+		$bytesOut = $bytes[64..($bytes.Count-1)];
+		
+		[System.IO.File]::WriteAllBytes(($_.FullName + '.aqo'), $bytesOut);
+	}
 
-$bytesOut = $bytes[64..($bytes.Count-1)];
-
-[System.IO.File]::WriteAllBytes(($_.FullName + '.aqo'), $bytesOut);
-}
+Get-ChildItem -Path $path -Filter *.aqp | 
+	Foreach-Object {
+		$PSItem.FullName
+		$bytes = [System.IO.File]::ReadAllBytes($PSItem.FullName);
+		
+		$bytesOut = $bytes[64..($bytes.Count-1)];
+		
+		[System.IO.File]::WriteAllBytes(($_.FullName + '.aqo'), $bytesOut);
+	}
